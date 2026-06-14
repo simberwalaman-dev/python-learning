d622a5b485212d2e93a8fe9f05f56b0238926632
Aman Kumar <simberwalaman@gmail.com>
2026-06-14 14:16:08 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+
+if b != 0:
+    print("Division =", a / b)
+else:
+    print("Cannot divide by zero")
